93e3764bc11333bad9d404ca858d0ada52d7f026
tr4vLer <149298759+tr4vLer@users.noreply.github.com>
2024-02-19 18:47:13 +0100

    Update start_app.ps1

--- start_app.ps1
+++ start_app.ps1
@@ -1,10 +1,10 @@
 # start_app.ps1
 
 # Start the Python application and keep the window open
-python "%~dp0app.py"
+Start-Process "python" -ArgumentList "`"$PSScriptRoot\app.py`""
 
 # Wait for the server to start
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 2
 
 # Open the default web browser
 Start-Process "http://127.0.0.1:4999"
