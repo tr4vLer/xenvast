d602a2597a20a0e17f99308a1d031502a155f59c
tr4vLer <149298759+tr4vLer@users.noreply.github.com>
2024-02-19 18:37:05 +0100

    Update start_app.ps1

--- start_app.ps1
+++ start_app.ps1
@@ -1,8 +1,8 @@
 # start_app.ps1
 # This assumes that the current directory is the $XENVAST_DIR
 
-# Start the Python application
-Start-Process "python" -ArgumentList "app.py"
+# Start the Python application and do not close the window
+python app.py
 
 # Wait for the server to start
 Start-Sleep -Seconds 2
