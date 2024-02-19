ca040ef092912e9ca1de9634bf6d590ecebbf6c1
tr4vLer <149298759+tr4vLer@users.noreply.github.com>
2024-02-19 18:42:41 +0100

    Update start_app.ps1

--- start_app.ps1
+++ start_app.ps1
@@ -1,11 +1,10 @@
 # start_app.ps1
-# This assumes that the current directory is the $XENVAST_DIR
 
-# Start the Python application and do not close the window
-python app.py
+# Start the Python application and keep the window open
+python "%~dp0app.py"
 
 # Wait for the server to start
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
 # Open the default web browser
 Start-Process "http://127.0.0.1:4999"
